452f628a9142c06b1319f16d67717d98a6fe295b
Modiss <130850939+Modiss3@users.noreply.github.com>
2024-11-06 22:16:00 +0300

    Update task_2.py

--- Лабораторная работа #3/task_2.py
+++ Лабораторная работа #3/task_2.py
@@ -1,9 +1,9 @@
 # TODO Напишите функцию find_common_participants
 def find_common_participants(first_group, seconds_group, split_sign=','):
-    first_group_set = set(first_group.split(split_sign))
-    second_group_list = seconds_group.split(split_sign)
-    common_participants_list = list(first_group_set.intersection(second_group_list))
-    common_participants_list.sort()
+    first_group_set = set(first_group.split(split_sign)) #создаем список на основе строки участников и переводим в множество, т.к. метод intersection() принадлежит ему
+    second_group_list = seconds_group.split(split_sign) #создаем список на основе строки участников
+    common_participants_list = list(first_group_set.intersection(second_group_list)) #создаем список из повторных участников
+    common_participants_list.sort() #сортируем список повторяющихся участников
     return common_participants_list
 
 participants_first_group = "Иванов|Петров|Сидоров"
